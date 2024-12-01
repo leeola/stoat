95b75d7107da32cd635f2e86956a0bc7379a5a30
Lee Olayvar <leegit@fastmail.com>
2024-12-01 11:56:10 -0800

    feat(core): thinking through config

--- core/src/config.rs
+++ core/src/config.rs
@@ -1,2 +1,13 @@
-pub struct Config;
-// TODO: impl loading over generic FS.
+use std::collections::BTreeMap;
+
+#[derive(Debug, Clone, PartialEq, Eq, PartialOrd, Ord, serde::Serialize, serde::Deserialize)]
+pub struct Config(pub ConfigMap);
+
+/// A limited [`Value`](crate::value::Value)-like for Config values.
+#[derive(Debug, Clone, PartialEq, Eq, PartialOrd, Ord, serde::Serialize, serde::Deserialize)]
+pub enum ConfigValue {
+    String(String),
+}
+
+#[derive(Debug, Clone, PartialEq, Eq, PartialOrd, Ord, serde::Serialize, serde::Deserialize)]
+pub struct ConfigMap(BTreeMap<String, ConfigValue>);
--- core/src/value.rs
+++ core/src/value.rs
@@ -1,5 +1,10 @@
+use std::collections::BTreeMap;
+
 /// A centralized data format used in Stoat for.. everything.
 #[derive(Debug, Clone, PartialEq, Eq, PartialOrd, Ord, serde::Serialize, serde::Deserialize)]
 pub enum Value {
     String(String),
 }
+
+#[derive(Debug, Clone, PartialEq, Eq, PartialOrd, Ord, serde::Serialize, serde::Deserialize)]
+pub struct Map<K, V = Value>(BTreeMap<K, V>);
