be10f08e9fb21487401b9553719c9dda0e37b33e
Lee Olayvar <leegit@fastmail.com>
2025-04-12 09:10:33 -0700

    refactor(core): disable mouse input
    For now.. not sure on the desired abstraction.

--- core/src/input/user.rs
+++ core/src/input/user.rs
@@ -10,12 +10,16 @@
 pub enum UserInput {
     // NIT: multiple chars?
     Keyboard(char),
-    Mouse(Mouse),
+    // Mouse(Mouse),
 }
 
-#[derive(Debug, Copy, Clone, PartialEq, Eq, PartialOrd, Ord)]
-pub enum Mouse {
-    Click { x: usize, y: usize },
-    // NIT: Velocity? Note sure what data we get for mouse scroll.
-    Scroll { up: bool },
-}
+// NIT: Not sure what i want to do about mouse. Mouse input is useful but it has some association to
+// the graphical environment. It's difficult to abstract that correctly. However Stoat being Canvas
+// based means it has a coordinate space.. so perhaps there is a good overlap somewhere.
+// Plus of course it can scroll in text windows, possibly reveal canvas space, etc.
+// #[derive(Debug, Copy, Clone, PartialEq, Eq, PartialOrd, Ord)]
+// pub enum Mouse {
+//     Click { x: usize, y: usize },
+//     // NIT: Velocity? Note sure what data we get for mouse scroll.
+//     Scroll { up: bool },
+// }
