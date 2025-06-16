d454670534f5941b05b3366e4c54d948e59a898b
Lee Olayvar <leegit@fastmail.com>
2025-06-15 21:59:44 -0700

    chore: rust 1.87.0

--- flake.nix
+++ flake.nix
@@ -24,7 +24,7 @@
             gcc
             rust-analyzer
             # using a hardcoded rustfmt version to support nightly rustfmt features.
-            rust-bin.nightly."2025-04-03".rustfmt
+            rust-bin.nightly."2025-05-14".rustfmt
             rust-toolchain
 
           ] ++ pkgs.lib.optionals pkgs.stdenv.isLinux [
