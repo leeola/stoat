ead945ca5b3207d9c0f52bf71cebe3710e2be8d6
Lee Olayvar <leegit@fastmail.com>
2025-07-02 14:34:02 -0700

    chore: update rust 1.88.0

--- flake.nix
+++ flake.nix
@@ -1,12 +1,19 @@
 {
   inputs = {
-    nixpkgs.url      = "github:nixos/nixpkgs/nixos-unstable";
+    nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable";
     rust-overlay.url = "github:oxalica/rust-overlay";
-    flake-utils.url  = "github:numtide/flake-utils";
+    flake-utils.url = "github:numtide/flake-utils";
   };
 
-  outputs = { nixpkgs, rust-overlay, flake-utils, ... }:
-    flake-utils.lib.eachDefaultSystem (system:
+  outputs =
+    {
+      nixpkgs,
+      rust-overlay,
+      flake-utils,
+      ...
+    }:
+    flake-utils.lib.eachDefaultSystem (
+      system:
       let
         overlays = [ (import rust-overlay) ];
         pkgs = import nixpkgs {
@@ -18,23 +25,27 @@
       in
       {
         devShell = pkgs.mkShell rec {
-          buildInputs = with pkgs; [
-            pkg-config
-            binutils
-            gcc
-            rust-analyzer
-            # using a hardcoded rustfmt version to support nightly rustfmt features.
-            rust-bin.nightly."2025-05-14".rustfmt
-            rust-toolchain
+          buildInputs =
+            with pkgs;
+            [
+              pkg-config
+              binutils
+              gcc
+              rust-analyzer
+              # using a hardcoded rustfmt version to support nightly rustfmt features.
+              rust-bin.nightly."2025-06-26".rustfmt
+              rust-toolchain
 
-          ] ++ pkgs.lib.optionals pkgs.stdenv.isLinux [
-          ] ++ pkgs.lib.optionals pkgs.stdenv.isDarwin [
-            darwin.apple_sdk.frameworks.Security
-            darwin.apple_sdk.frameworks.Foundation
-            darwin.apple_sdk.frameworks.Cocoa
-            darwin.apple_sdk.frameworks.Carbon
-            darwin.apple_sdk.frameworks.WebKit
-          ];
+            ]
+            ++ pkgs.lib.optionals pkgs.stdenv.isLinux [
+            ]
+            ++ pkgs.lib.optionals pkgs.stdenv.isDarwin [
+              darwin.apple_sdk.frameworks.Security
+              darwin.apple_sdk.frameworks.Foundation
+              darwin.apple_sdk.frameworks.Cocoa
+              darwin.apple_sdk.frameworks.Carbon
+              darwin.apple_sdk.frameworks.WebKit
+            ];
 
           # necessary for some libs. Not even sure which ones,
           # i just copy and pasted from a Bevy repo of mine, hah.
