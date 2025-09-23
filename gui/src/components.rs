99e55be1d3491d85d168cbbc0f08f371a2a34ed6
Lee Olayvar <leegit@fastmail.com>
2025-09-23 06:06:14 -0700

    wip, remove stoat/gui

--- gui/src/components.rs
+++
@@ -1,6 +0,0 @@
-//! Reusable UI components for the Stoat editor.
-//!
-//! This module contains self-contained, styled components that follow
-//! GPUI patterns for composability and theming.
-
-pub mod command_panel;
--- stoat/src/config.rs
+++
@@ -1,5 +0,0 @@
-//! Configuration structures for the editor.
-
-pub mod keymap;
-
-pub use keymap::{KeyBinding, KeymapConfig, ModeConfig};
