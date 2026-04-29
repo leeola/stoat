b7cbee47fe31eac2247c57dacf0f188b80e0980e
Lee Olayvar <leegit@fastmail.com>
2026-04-29 09:12:03 -0700

    chore(flake): track nixpkgs master instead of nixos-unstable
    Point the nixpkgs input at the master branch and refresh the lock
    to the matching revision.

--- flake.nix
+++ flake.nix
@@ -1,6 +1,6 @@
 {
   inputs = {
-    nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable";
+    nixpkgs.url = "github:nixos/nixpkgs/master";
     rust-overlay.url = "github:oxalica/rust-overlay";
     flake-utils.url = "github:numtide/flake-utils";
   };
